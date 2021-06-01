d26efd6a4efe9ad377083dffaa104cff90612b9b
Veerabharathi Vijayaraghavan <veera.v@invenzolabs.com>
2021-06-01 16:59:10 +0530

    'update'

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -1,3 +1,3 @@
-<a href="{{'email-by-listner'}}"><input type="button" class="btn btn-primary" value="Email By Listner"></a>
+<a href="{{'email-by-listner'}}"><input type="button" class="btn btn-primary" value="Email By Listener"></a>
 
-<a href="{{'email-by-queue-listner'}}"><input type="button" class="btn btn-primary" value="Email By Queue Listner"></a>
+<a href="{{'email-by-queue-listner'}}"><input type="button" class="btn btn-primary" value="Email By Queue Listener"></a>
